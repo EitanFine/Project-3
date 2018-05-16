e337996db463901bee930d18f26cb1da74a504bb
alexbaranov43 <alexbaranov43@gmail.com>
2018-05-16 14:05:25 -0400

    app.js displays components

--- rent_my_stuff/src/App.js
+++ rent_my_stuff/src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ListingCard from "./components/ListingCard"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={} />
-        <Route exact path="/category" component={Category} />
-        <Route exact path="/category/:id" component={Category} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
+      <ListingCard />
     );
   }
 }
