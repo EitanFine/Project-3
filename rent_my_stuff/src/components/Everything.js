56e4731adf92274aec699dac1f64e803dae816a9
Lan Ejiri <lan.ejiri@yahoo.com>
2018-05-21 13:46:45 -0400

    changing home page

--- rent_my_stuff/src/components/Everything.js
+++ rent_my_stuff/src/components/Everything.js
@@ -17,82 +17,16 @@ class Everything extends Component {
   }
 
   renderStuff = () => {
-    return this.state.stuff.map(item => {
-        return (
-          <div>
-            <div className="container">
-              <div className="panel panel-default">
-                <div className="panel-heading">
-                  <h4>
-                    <b>Listing:</b> {item.itemName}
-                  </h4>
-                </div>
-                <div className="panel-body">
-                  <div className="row">
-                    <div className="col-sm-4">
-                      <img
-                        style={{
-                          height: "225px",
-                          width: "90%",
-                          borderRadius: "5%"
-                        }}
-                        src={item.itemURL}
-                        alt="img"
-                      />
-                    </div>
-                    <div className="col-sm-8">
-                      <div className="row">
-                        <div className="col-sm-6">
-                          <h4>
-                            <b>Listing Id:</b> {item.id}.
-                          </h4>
-                          <h4>
-                            <b>Price:</b> ${item.itemPrice}.{" "}
-                            <span
-                              style={{
-                                color: "#626262",
-                                fontFamily: "'Timmana', sans-serif"
-                              }}
-                            >
-                              (per day)
-                            </span>
-                          </h4>
-                        </div>
-                        <div className="col-sm-6 text-right">
-                          <Link to={`/singleitem/${item.id}`}>
-                            <button className="btn btn-lg">View Listing</button>
-                          </Link>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-sm-12">
-                          <hr />
-                          <h4>
-                            <b>Description</b>
-                          </h4>
-                          <p>{item.itemDescription}.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-12 text-right">
-                      <h5
-                        style={{ fontFamily: "'Timmana', sans-serif" }}
-                        className="text-right"
-                      >
-                        <span>Posted On: </span>
-                        {Moment(item.createdAt).format("LL")}
-                      </h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      
-    });
+    return (
+      <div>
+        <Link to="/rentals">
+          <h1>go to rentals, put picture here</h1>
+        </Link>
+        <Link to="/services">
+          <h1>go to services, put picture here</h1>
+        </Link>
+      </div>
+    );
   };
 
   render() {
