00d3619eeb4ba531a5ec54ab931f3eefac5f70f2
Eitan Fine <eitanmfine@gmail.com>
2018-05-23 15:33:31 -0400

    leave comment

--- rent_my_stuff/src/components/SingleItem.js
+++ rent_my_stuff/src/components/SingleItem.js
@@ -255,7 +255,7 @@ class SingleItem extends Component {
                                 <CommentDisplay comments={this.state.Comments}  />
                             </div>
 
-                            <strong>  Leave a Comment</strong>
+                            {/* <strong>  Leave a Comment</strong> */}
                         </div>
                     </div>
 
