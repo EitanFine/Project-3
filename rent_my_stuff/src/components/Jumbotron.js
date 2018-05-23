cb4aa44761b70a4b88e2b5f8beeb706d00157271
Inomzhon Ibragimov <mail4inom@users.noreply.github.com>
2018-05-23 13:49:15 -0400

    jumbotron moved to be a component

---
+++ rent_my_stuff/src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+
+class Jumbotron extends React.Component {
+    render() {
+        return (
+            <div style={{ background: "white" }} class="jumbotron text-center">
+                <Link to="/"><img style={{ width: "186px", marginTop: "32px", marginBottom: "-46px" }} src={ logo } alt="logo" /></Link>
+            </div>
+        )
+    }
+}
+export default Jumbotron;
