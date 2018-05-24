3839994d2c874ec94413f807cf472c852ea64810
alexbaranov43 <alexbaranov43@gmail.com>
2018-05-24 11:17:48 -0400

    merge

--- rent_my_stuff/src/components/Navbar.js
+++ rent_my_stuff/src/components/Navbar.js
@@ -5,41 +5,6 @@ import logo from './logo.svg';
 const Navbar = ({ loggedIn, logout }) => {
   return (
     <div>
-<<<<<<< HEAD
-    <nav className="navbar navbar-default navbar-fixed-top">
-    
-      <div className="container">
-        <div className="navbar-header">
-        <br/>
-          <Link className='navText'  to="/"><span  className="glyphicon glyphicon-home"></span></Link>
-        </div>
-        <ul className="nav navbar-nav">
-          <br />
-          <li><Link style={{color: 'white'}} className='navText' to="/rentals">Rentals</Link></li>
-          <li><Link style={{color: 'white'}} className='navText' to="/services">Services</Link></li>
-          <li><Link style={{color: 'white'}} className='navText' to="/About">About</Link></li>
-          <li><Link style={{color: 'white'}} className='navText' to="/HowItWorks">How It Works</Link></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          {loggedIn ?
-            [
-              <br />,
-              <li className='navText' key={1}><Link style={{color: 'white'}}  className="nav-item nav-link" to="/MyItems">My Account</Link></li>,
-              <li className='navText' key={2}><Link style={{color: 'white'}}  className="nav-item nav-link" to="/PostListing">Post Listing</Link></li>,
-              <li className='navText' key={3}><a style={{color: 'white'}} className='navText' className="nav-item nav-link" href="/" onClick={logout}>Logout</a></li>
-            ]
-            :
-            [
-              <li><Link style={{color: 'white'}} className='navText' to="/signup"><span className="glyphicon glyphicon-user"></span>Signup</Link></li>,
-              <li><Link style={{color: 'white'}} className='navText' to="/login"><span className="glyphicon glyphicon-log-in"></span>Login</Link></li>
-            ]
-          }
-        </ul>
-      </div>
-     <br/>
-    </nav>
-   
-=======
       <nav className="navbar navbar-default navbar-fixed-top navbar-collapse">
         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
           <span className="sr-only">Toggle navigation</span>
@@ -80,7 +45,6 @@ const Navbar = ({ loggedIn, logout }) => {
         </div>
       </nav>
 
->>>>>>> alex
     </div>
   );
 }
