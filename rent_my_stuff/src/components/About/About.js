1d20e1690008c7f5b6f181fdd487f5d28f1da75c
Eitan Fine <eitanmfine@gmail.com>
2018-05-24 12:44:19 -0400

    mobile responsive about page

--- rent_my_stuff/src/components/About/About.js
+++ rent_my_stuff/src/components/About/About.js
@@ -14,124 +14,111 @@ class About extends React.Component {
               <br />
             </div>
             <div className="row">
-              <div className="col-md-4 col-xs-4 center">
-                <div className="caption">
-                  <img
-                    className="ah mix-auto rounded-circle"
-                    src="./images/alex.png"
-                    alt="..."
-                  />
+              <div class="col-md-4">
 
-                  <h3 className="team">Alex Baranov</h3>
-                  <p>Full Stack Web Developer</p>
-                  <p>
-                    <a href="https://github.com/alexbaranov43" target="_blank">
-                      <i className="fab fa-github" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/alex-baranov-285b31159/" target="_blank">
-                      <i className="fab fa-linkedin" />
-                    </a>
-                    <a href="https://www.google.com/" target="_blank">
-                      <i className="fab fa-stack-overflow" />
-                    </a>
-                  </p>
-                </div>
+                <img
+                  className="ah mix-auto rounded-circle"
+                  src="./images/alex.png"
+                  alt="..."
+                />
+                <h3 className="team">Alex Baranov</h3>
+                <p>Full Stack Web Developer</p>
+                <p>
+                  <a href="https://github.com/alexbaranov43" target="_blank">
+                    <i className="fab fa-github" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/alex-baranov-285b31159/" target="_blank">
+                    <i className="fab fa-linkedin" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <i className="fab fa-stack-overflow" />
+                  </a>
+                </p>
               </div>
-              <div className="col-md-4 col-xs-4 center">
-                <div className="caption">
-                  <img
-                    className="ah mix-auto rounded-circle"
-                    src="./images/inom.png"
-                    alt="..."
-                  />
+              <div class="col-md-4">
 
-                  <h3 className="team">Inom Ibragimov</h3>
-                  <p>Full Stack Web Developer</p>
-                  <p>
-                    <a href="https://github.com/mail4inom" target="_blank">
-                      <i className="fab fa-github" />
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/inomzhon-ibragimov-0091b2156/" target="_blank">
-                      <i className="fab fa-linkedin" />
-                    </a>
-                    <a href="https://stackexchange.com/users/12854870/inomzhon-ibragimov" target="_blank">
-                      <i className="fab fa-stack-overflow" />
-                    </a>
-                  </p>
-                </div>
+                <img
+                  className="ah mix-auto rounded-circle"
+                  src="./images/inom.png"
+                  alt="..."
+                />
+                <h3 className="team">Inom Ibragimov</h3>
+                <p>Full Stack Web Developer</p>
+                <p>
+                  <a href="https://github.com/mail4inom" target="_blank">
+                    <i className="fab fa-github" />
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/inomzhon-ibragimov-0091b2156/" target="_blank">
+                    <i className="fab fa-linkedin" />
+                  </a>
+                  <a href="https://stackexchange.com/users/12854870/inomzhon-ibragimov" target="_blank">
+                    <i className="fab fa-stack-overflow" />
+                  </a>
+                </p>
+
               </div>
-              <div className="col-md-4 col-xs-4 center">
-                <div className="caption">
-                  <img
-                    className="ah mix-auto rounded-circle"
-                    src="./images/eitan.png"
-                    alt="..."
-                  />
-
-                  <h3 className="team">Eitan Fine</h3>
-                  <p>Full Stack Web Developer</p>
-                  <p>
-                    <a href="https://www.google.com/" target="_blank">
-                      <i className="fab fa-github" />
-                    </a>
-                    <a href="https://www.google.com/" target="_blank">
-                      <i className="fab fa-linkedin" />
-                    </a>
-                    <a href="https://www.google.com/" target="_blank">
-                      <i className="fab fa-stack-overflow" />
-                    </a>
-                  </p>
-                </div>
+              <div class="col-md-4">
+                <img
+                  className="ah mix-auto rounded-circle"
+                  src="./images/eitan.png"
+                  alt="..."
+                />
+                <h3 className="team">Eitan Fine</h3>
+                <p>Full Stack Web Developer</p>
+                <p>
+                  <a href="https://github.com/EitanFine" target="_blank">
+                    <i className="fab fa-github" />
+                  </a>
+                  <a href="www.linkedin.com/in/eitan-fine-9466b5158" target="_blank">
+                    <i className="fab fa-linkedin" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <i className="fab fa-stack-overflow" />
+                  </a>
+                </p>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-2 col-xs-2 " />
-              <div className="col-md-4 col-xs-4 center">
-                <div className="caption">
-                  <img
-                    className="ah mix-auto rounded-circle"
-                    src="./images/john.png"
-                    alt="..."
-                  />
+            <div className="row i-am-centered">
+              <div className="col-md-6">
 
-                  <h3 className="team">John Palumbo</h3>
-                  <p>Full Stack Web Developer</p>
-                  <p>
-                    <a href="https://www.google.com/" target="_blank">
-                      <i className="fab fa-github" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/john-palumbo-nj" target="_blank">
-                      <i className="fab fa-linkedin" />
-                    </a>
-                    <a href="https://www.google.com/" target="_blank">
-                      <i className="fab fa-stack-overflow" />
-                    </a>
-                  </p>
-                </div>
+                <img
+                  className="ah mix-auto rounded-circle"
+                  src="./images/john.png"
+                  alt="..."
+                />
+                <h3 className="team">John Palumbo</h3>
+                <p>
+                  <a href="https://www.google.com/" target="_blank">
+                    <i className="fab fa-github" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/john-palumbo-nj" target="_blank">
+                    <i className="fab fa-linkedin" />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <i className="fab fa-stack-overflow" />
+                  </a>
+                </p>
               </div>
-              <div className="col-md-4 col-xs-4 center">
-                <div className="caption">
-                  <img
-                    className="ah mix-auto rounded-circle"
-                    src="./images/lan.png"
-                    alt="..."
-                  />
-
-                  <h3 className="team">Lan Ejiri</h3>
-                  <p>Full Stack Web Developer</p>
-                  <p>
-                    <a href="https://github.com/lan-ejiri" target="_blank">
-                      <i className="fab fa-github" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/lanejiri/" target="_blank">
-                      <i className="fab fa-linkedin" />
-                    </a>
-                    <a href="https://stackoverflow.com/users/9201899/lan" target="_blank">
-                      <i className="fab fa-stack-overflow" />
-                    </a>
-                  </p>
-                </div>
+              <div class="col-md-6">
+                <img
+                  className="ah mix-auto rounded-circle"
+                  src="./images/lan.png"
+                  alt="..."
+                />
+                <h3 className="team">Lan Ejiri</h3>
+                <p>Full Stack Web Developer</p>
+                <p>
+                  <a href="https://github.com/lan-ejiri" target="_blank">
+                    <i className="fab fa-github" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/lanejiri/" target="_blank">
+                    <i className="fab fa-linkedin" />
+                  </a>
+                  <a href="https://stackoverflow.com/users/9201899/lan" target="_blank">
+                    <i className="fab fa-stack-overflow" />
+                  </a>
+                </p>
               </div>
             </div>
           </div>
