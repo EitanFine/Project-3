989537c161aed2cce24a6f7fec959356568c9a75
alexbaranov43 <alexbaranov43@gmail.com>
2018-05-24 14:00:01 -0400

    checking environment

--- models/index.js
+++ models/index.js
@@ -5,11 +5,14 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-console.log("ENVIRONMENTL ", env);
+
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+console.log("Config: ", config);
+
 if (config.use_env_variable) {
+  console.log("ENVIRONMENT", process.env[config.use_env_variable]);
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
